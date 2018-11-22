cb92c8fdd2019d1450c8c0824adf87c98f7fb1c7
S3Blevins <s3.blevins@gmail.com>
2018-11-22 16:45:33 -0700

    Updated files

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # parallel_gcode
+
+To run the file use make clean, then make
+
+# To Install the library:
+
+sudo apt update
+
+sudo apt install cimg-dev
+
+# To Run graphics on external computer
+
+ssh name@server_ip -X 
--- kernel.h
+++ kernel.h
@@ -1,8 +1,36 @@
-//#include "cuda.h"
+#include "cuda.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream> // C++ I/O library
+#include "CImg.h"
 #include <vector>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include<fstream>
+#include<string>
 
 using namespace std;
+using namespace cimg_library;
 
-vector<int> edge_detection(int fd);
+// Standard Functions
+void edge_detection_wrapper(char flags, string input_name, string output_name, int threshold);
 
-void g_gen(int fd);
+vector<int> vectorize_img(CImg<unsigned char> img, int *width, int *height);
+
+vector<int> edge_detection_cpu(vector<int> img, int width, int height, int threshold);
+
+int display_img(vector<int> img, int width, int height, int write_flag, string output);
+
+int metadata(CImg<unsigned char> img, int threshold);
+
+// CUDA Functions
+void error_check(cudaError_t err);
+
+__global__
+void sobelFilterKernel(int *imageRGB, int width, int height, int *Gx_matrix, int *Gy_matrix);
+
+vector<int> edge_detection_gpu(vector<int> img, int width, int height, int threshold);
+
+// g-code Generator Functions
+void g_gen(vector<int> img, int width, int height);
